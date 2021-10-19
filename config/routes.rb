505dd0b1bbf803fa1413c6d30942c92aa5078d99
Ivan Candido <ivan.candido@powerhrg.com>
2021-10-19 09:41:21 -0400

    Added devise player model for loggins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :players
   resources :lobbies
   resources :users
   resources :leaderboards, only: :index
--- test/models/player_in_lobby_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class PlayerInLobbyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
