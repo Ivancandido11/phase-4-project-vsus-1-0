33cf313b1a9b5533eb14b09ad80f5196bb030a3a
Ivan Candido <ivan.candido@powerhrg.com>
2021-10-20 16:49:38 -0400

    Deleted unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resource :user_in_lobbies, only: :create
-  resources :lobbies
-  resources :users
+  resources :lobbies, only: %i[index show new create destroy]
+  resources :users, only: %i[show new create edit update]
   resources :leaderboards, only: :index
-  resources :admin
+  resources :admin, only: :index
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   post "logout", to: "sessions#destroy"
