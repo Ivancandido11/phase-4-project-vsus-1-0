e4690f320913740b6e0fc4f615a447903da88b28
Ivan Candido <ivan.candido@powerhrg.com>
2021-10-26 14:26:18 -0400

    Added tests for Lobby associations

--- spec/models/lobby_spec.rb
+++ spec/models/lobby_spec.rb
@@ -1,3 +1,19 @@
-RSpec.describe Lobby, type: :model do
+require "rails_helper"
 
+RSpec.describe Lobby, type: :model do
+  describe "should have many user_in_lobbies" do
+    it { should have_many(:user_in_lobbies).dependent(:destroy) }
+  end
+
+  describe "should have many users" do
+    it { should have_many(:users).through(:user_in_lobbies) }
+  end
+
+  describe "should belong to a host, through the class_name User" do
+    it { should belong_to(:host).class_name('User') }
+  end
+
+  describe "should accept nested attributes for user_in_lobbies" do
+    it { should accept_nested_attributes_for(:user_in_lobbies) }
+  end
 end
