e42f8f95939fc5b2bb468b08e28531c323a8ff49
Ivan Candido <ivan.candido@powerhrg.com>
2021-10-29 08:43:45 -0400

    Added controller specs for leaderboards

--- spec/controllers/leaderboards_spec.rb
+++ spec/controllers/leaderboards_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "Leaderboards", type: :request do
+RSpec.describe LeaderboardsController, type: :controller do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "grabs the top 25 users based on points" do
+      user = create(:user)
+      create_users
+      users = User.order(points: :desc).limit(25)
+
+      get :index
+
+      expect(assigns[:users]).not_to include(user)
+      expect(assigns[:users]).to eq users
+    end
+
+    it "renders the index template" do
+      get :index
+      
+      expect(response).to render_template("index")
+    end
+  end
+
+  def create_users
+    40.times do
+      create(:user, username: Faker::Name.unique.name, email: Faker::Internet.unique.email, points: rand(10..80))
+    end
   end
 end
